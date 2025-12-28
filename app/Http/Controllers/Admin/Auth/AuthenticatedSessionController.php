6d2d74c1e444c94dde978d68737573e787b993ec
kemini0615 <moeday1010@gmail.com>
2025-12-29 01:07:36 +0900

    Laravel Breeze 스캐폴딩을 재활용하여 관리자 인증 라우트, 컨트롤러, 뷰 추가

---
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * 로그인 페이지를 보여준다.
+     */
+    public function create(): View
+    {
+        return view('admin.auth.login');
+    }
+
+    /**
+     * 로그인(인증) 요청을 처리한다.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(route('dashboard', absolute: false));
+    }
+
+    /**
+     * 인증 세션을 삭제한다.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
