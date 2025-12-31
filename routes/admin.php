c86bbcbeeabf066935018baa5cd76bcb43e0d864
kemini0615 <moeday1010@gmail.com>
2025-12-31 17:44:54 +0900

    불필요한 파일과 디렉토리 삭제

--- routes/admin.php
+++ routes/admin.php
@@ -1,25 +1,19 @@
 <?php
 
 use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Admin\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Admin\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Admin\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Admin\Auth\NewPasswordController;
-use App\Http\Controllers\Admin\Auth\PasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Admin\Auth\RegisteredUserController;
-use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+
+// ADMIN DASHBOARD
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard.index');
+})->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
 
 Route::middleware('guest:admin')
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
-        Route::get('register', [RegisteredUserController::class, 'create'])
-            ->name('register');
-
-        Route::post('register', [RegisteredUserController::class, 'store']);
-
         Route::get('login', [AuthenticatedSessionController::class, 'create'])
             ->name('login');
 
@@ -42,24 +36,6 @@ Route::middleware('auth:admin')
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
-        Route::get('verify-email', EmailVerificationPromptController::class)
-            ->name('verification.notice');
-
-        Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-            ->middleware(['signed', 'throttle:6,1'])
-            ->name('verification.verify');
-
-        Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-            ->middleware('throttle:6,1')
-            ->name('verification.send');
-
-        Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-            ->name('password.confirm');
-
-        Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-
-        Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-
         Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
             ->name('logout');
     });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\User\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,18 +11,6 @@ Route::get('/', function () {
 // USER DASHBOARD
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-});
-
-// ADMIN DASHBOARD
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard.index');
-})->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
-
-// PROFILE
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
